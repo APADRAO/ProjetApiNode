e243eb6d7371eb51ea40ea8ee8e12d5a3b4fba08
Alexandre Padrao <alexandre.padrao@dbschenker.com>
2023-05-15 21:28:58 -0300

    .

--- src/app/controllers/UserControllers.ts
+++ src/app/controllers/UserControllers.ts
@@ -11,100 +11,13 @@ const SECRET_KEY = "abrakadabra";
 
 const userRouter = Router();
 
-
-/**
- * @swagger
- * /api/users:
- *   get:
- *     summary: Get all users
- *     responses:
- *       '200':
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/User'
- */
-userRouter.get('/users', async ( _req:Request, res:Response): Promise<Response>=>{
-    console.log('INICIANDO GET ALL');
-    const users = await UserRepositorie.getUsers();
-    return res.status(200).json(users);
-});
-
-/**
- * @swagger
- * /api/users/by:
- *   get:
- *     summary: Get user by param
- *     parameters:  
- *       - in: query
- *         name: param
- *         required: true
- *         schema:
- *           type: string
- *         description: busca por nome, email ou password
- *     responses:
- *       '200':
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *                 $ref: '#/components/schemas/User'
- */
-userRouter.get('/users/by', async (request:Request, response:Response): Promise<Response>=>{
-    console.log('Iniciando Get com Parametros');
-
-    const {param} = request.query
-    const user = await UserRepositorie.getUserByParam({param}.param)
-    if(user instanceof Error){
-        return response.status(400).json();
-    }
-    return response.json(user);
-})
-
-/**
- * @swagger
- * /api/users:
- *   post:
- *     summary: Create a new user
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *     responses:
- *       '201':
- *         description: User created successfully
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       '400':
- *         description: Bad request
- *       '500':
- *         description: Internal server error
- */
-userRouter.post('/users',async  ( request:Request, response:Response): Promise<Response>=>{
-    var {  name, email, password } = request.body;
-   
-    //password = encrypt(password);
-    console.log("NOME: ", name);
-    const result = await UserRepositorie.postUser({ name, email, password })
-    if(result instanceof Error){
-        return response.status(400).json();
-    }
-    return response.json(result);
-});
-
-
 /**
  * @swagger
  * /api/login:
  *   post:
  *     summary: Gera JWT
+ *     tags:
+ *       - Autentication
  *     requestBody:
  *       required: true
  *       content:
@@ -141,5 +54,107 @@ userRouter.post("/login", async (req: Request, res: Response): Promise<Return> =
     }
     return ret
 });
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get all users
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+userRouter.get('/users', async ( _req:Request, res:Response): Promise<Response>=>{
+    console.log('INICIANDO GET ALL');
+    const users = await UserRepositorie.getUsers();
+    return res.status(200).json(users);
+});
+
+/**
+ * @swagger
+ * /api/users/by:
+ *   get:
+ *     summary: Get user by param
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:  
+ *       - in: query
+ *         name: param
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: busca por nome, email ou password
+ *     responses:
+ *       '200':
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *                 $ref: '#/components/schemas/User'
+ */
+userRouter.get('/users/by', async (request:Request, response:Response): Promise<Response>=>{
+    console.log('Iniciando Get com Parametros');
+
+    const {param} = request.query
+    const user = await UserRepositorie.getUserByParam({param}.param)
+    if(user instanceof Error){
+        return response.status(400).json();
+    }
+    return response.json(user);
+})
+
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       '201':
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
+userRouter.post('/users',async  ( request:Request, response:Response): Promise<Response>=>{
+    var {  name, email, password } = request.body;
+   
+    //password = encrypt(password);
+    console.log("NOME: ", name);
+    const result = await UserRepositorie.postUser({ name, email, password })
+    if(result instanceof Error){
+        return response.status(400).json();
+    }
+    return response.json(result);
+});
+
+
+
 
 export default userRouter;
--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,9 @@ import routers from './app/routes/routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import jwt from "jsonwebtoken";
-import * as swaggerFile from './swagger.json';
 import * as path from 'path';
-import { authRouter } from "./authRouter";
-import { authenticateJWT } from "./authMiddleware";
 
-
+const port = 3333;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -24,30 +21,40 @@ const options = {
       title: 'API Documentation',
       version: '1.0.0',
       description: 'Documentation for your API',
-    }, 
-    securityDefinitions: {
-      BearerAuth: {
-        type: 'apiKey',
-        name: 'Authorization',
-        in: 'header',
+    },  
+  component:{
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
       },
     },
   },
+  security:[
+    {
+      bearerAuth:[]
+    }
+  ]
+},
   apis: [path.join(__dirname, './**/*.ts')], // Caminho para seus arquivos com as anotações JSDoc
+  
 };
 
 const specs = swaggerJsdoc(options);
-// Adicione o middleware do Swagger UI com as configurações
+ //swagger page
+ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs)); 
 
-
-
-// Configurar a rota para a documentação do Swagger
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs)); // Aplicar o middleware de autenticação antes de exibir a documentação
-
+ //doc JsonFormat
+    /*app.get('docs.json', (req:Request, res:Response)=>{
+        res.setHeader('Content-Type', 'aplication/json');
+        res.send(specs);
+    });*/
 
 AppDataSource.initialize().then(async () => [
   console.log("DataBase : OK"),
-  app.listen(3333, () => {
-    console.log("API inicializada em porta 3333");
+  app.listen(port, () => {
+    
+    console.log(`API inicializada em: http://localhost:${port}/swagger`);
   })
 ]);
